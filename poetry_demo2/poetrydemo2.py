311f01b9fe2f07a3619aba9b15cbf0a447b97abe
macurandb <macurandb@gmail.com>
2021-02-10 10:18:36 -0500

    Added repository

---
+++ poetry_demo2/poetrydemo2.py
@@ -0,0 +1,9 @@
+
+
+
+def poetrydemo2():
+    return "PoetryDemo2"
+
+
+if __name__ == "__main__":
+    print(poetrydemo2())
---
+++ tests/test_poetry_demo2.py
@@ -0,0 +1,6 @@
+from poetry_demo2 import __version__
+from poetry_demo2 import poetrydemo2
+
+
+def test_version():
+    assert poetrydemo2.poetrydemo2() == "PoetryDemo2"
